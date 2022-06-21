db3bf22b9d6af421418c07782a524cb7a646c58b
Sahu, Rakesh Kumar (BSH) <sahu.rakesh-ext@bshg.com>
2022-06-21 19:54:00 +0530

    added failing test cases

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
+#include <iomanip>
+#include <cstring>
 #include <assert.h>
+using namespace std;
+int getMaxWidth(const char* color[], int size) {
+    int maxLength = 0;
+    for (int i = 0; i < size; ++i)
+    {
+        int length = strlen(color[i]); 
+        if (maxLength < length)
+        {
+            maxLength = length;
+        }
+    }
+    return maxLength;
+}
 
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    int maxMajorColorLen = getMaxWidth(majorColor, 5);
+    int maxMinorColorLen = getMaxWidth(minorColor, 5);
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
-        for(j = 0; j < 5; j++) {
-            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
+        for(j = 1; j <= 5; j++) {
+            std::cout << std::setw(4) << left << i * 5 + j << " | " << std::setw(maxMajorColorLen) << left <<  majorColor[i] << " | " << std::setw(maxMinorColorLen) << minorColor[i] << "\n";
         }
     }
     return i * j;
