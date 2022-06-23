51141fbc98e5cbb7bb16fcbfff8a4c5cfd8e6d9d
Sahu, Rakesh Kumar (BSH) <sahu.rakesh-ext@bshg.com>
2022-06-23 17:30:53 +0530

    Addressed review comments

--- alerter.cpp
+++ alerter.cpp
@@ -25,9 +25,10 @@ void alertInCelcius(float farenheit) {
 
 int main() {
     alertInCelcius(400.5);
+    assert(alertFailureCount == 0);
     alertInCelcius(303.6);
+    assert(alertFailureCount == 1);
     std::cout << alertFailureCount << " alerts failed.\n";
-    assert(alertFailureCount != 0);
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
--- misaligned.cpp
+++ misaligned.cpp
@@ -1,32 +1,32 @@
 #include <iostream>
-#include <iomanip>
 #include <cstring>
 #include <assert.h>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
-int getMaxWidth(const char* color[], int size) {
-    int maxLength = 0;
-    for (int i = 0; i < size; ++i)
-    {
-        int length = strlen(color[i]); 
-        if (maxLength < length)
-        {
-            maxLength = length;
-        }
-    }
-    return maxLength;
-}
 
 int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    int maxMajorColorLen = getMaxWidth(majorColor, 5);
-    int maxMinorColorLen = getMaxWidth(minorColor, 5);
+    const char* majorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
+    const char* minorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    int majorColorLen = (sizeof(majorColors) / sizeof(majorColors[0]));
+    int minorColorLen = (sizeof(minorColors) / sizeof(minorColors[0]));
+    vector<pair<string, string>> colorPair;
     int i = 0, j = 0;
-    for(i = 0; i < 5; i++) {
-        for(j = 1; j <= 5; j++) {
-            std::cout << std::setw(4) << left << i * 5 + j << " | " << std::setw(maxMajorColorLen) << left <<  majorColor[i] << " | " << std::setw(maxMinorColorLen) << minorColor[i] << "\n";
+    for(i = 0; i < majorColorLen; i++) {
+        for(j = 0; j < minorColorLen; j++) {
+            string majorColor = majorColors[i];
+            string minorColor = minorColors[i];
+            std::cout <<  i * 5 + j << " | " <<  majorColor << " | " <<  minorColor << "\n";
+            pair<string, string> toFind(majorColor, minorColor);
+            auto itr = std::find_if(colorPair.begin(), colorPair.end(), [&toFind](pair<string, string> p){
+               return p.first == toFind.first && p.second == toFind.second;
+            });
+            assert (itr != colorPair.end());
+            colorPair.emplace_back(make_pair(majorColor, minorColor));
         }
     }
+    
     return i * j;
 }
 
