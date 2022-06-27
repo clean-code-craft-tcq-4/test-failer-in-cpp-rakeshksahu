ba7b266219b57c79875f999bb53e037d53b22b07
Sahu, Rakesh Kumar (BSH) <sahu.rakesh-ext@bshg.com>
2022-06-27 15:46:33 +0530

    fixing tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -3,9 +3,9 @@
 
 char size(int cms) {
     char sizeName = '\0';
-    if(cms < 38) {
+    if(cms <= 38) {
         sizeName = 'S';
-    } else if(cms > 38 && cms < 42) {
+    } else if(cms > 38 && cms <= 42) {
         sizeName = 'M';
     } else if(cms > 42) {
         sizeName = 'L';
@@ -19,7 +19,6 @@ int main() {
     assert(size(40) == 'M');
     assert(size(42) == 'M');
     assert(size(43) == 'L');
-    assert(size(100) == 'L');
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
